c18f1f305c1770c0c8976e9790d50e7391e44163
kazimlaos <1LAOSK42@solent.ac.uk>
2024-06-12 22:37:34 +0100

    adding the last files for version 1

--- Website/index.js
+++ Website/index.js
@@ -15,8 +15,6 @@ var cookieParser= require('cookie-parser');
 
 
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const JWT_SECRET = '2f78e0664e0f2d86d15e8461d9362c832f0b7582532c71b45d4138d40b60dab9dd20c62b43d6e4ff9e4953c29e8b9ad94e2b1658e45d91bf77aa6c3a19075f06';
 const User = require('./model/User'); 
 // /* const morgan = require('morgan');
 //  */
@@ -62,26 +60,8 @@ app.set('views', path.join(__dirname, 'views/pages'));
 
 
 //Verify Authentication
-const authenticateJWT = (req, res, next) => {
-  //   console.log("showing request");
-  //   console.log(req.headers);
-  //   const token = req.headers['authorization'];
-
-  //   console.log(token);
-  //   console.log(JWT_SECRET);
-  // if (token) {
-  //   jwt.verify(token, JWT_SECRET, (err, user) => {
-  //     if (err) {
-  //       return res.sendStatus(403);
-  //     }
-  //     req.user = user;
-  //     next();
-  //   });
-  // } else {
-  //     console.log("Token verification failed");
-  //   res.sendStatus(401);
-  // }
-    if(req.session.authenticated){
+const authenticateSession = (req, res, next) => {
+     if(req.session.authenticated){
 //	req.user = user;
 	next();
     } else {
@@ -109,14 +89,14 @@ app.get('/login', (req, res) => {
 });
 
 // Route to render search page
-app.get('/search', authenticateJWT, (req, res) => {
+app.get('/search', authenticateSession, (req, res) => {
 //app.get('/search', (req, res) => {
   res.render('search');
 });
 
 
 // Route to render search page
-app.get('/filteredSearch', authenticateJWT, (req, res) => {
+app.get('/filteredSearch', authenticateSession, (req, res) => {
 //app.get('/search', (req, res) => {
   res.render('filteredSearch');
 });
@@ -153,10 +133,10 @@ app.post('/login', async (req, res) => {
       return res.status(400).send({ message: 'Invalid username or password' });
     }
       req.session.authenticated=true;
-      const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '1h' });
+      //const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '1h' });
       //res.status(200).j({message: 'Yeah'});
       //res.send({ token: token });
-      res.status(200).json({token});
+      res.status(200).send({ message: 'Logging OK' });
   } catch (err) {
     console.log(err);
     res.status(500).send({ message: 'Error logging in' });
@@ -164,6 +144,17 @@ app.post('/login', async (req, res) => {
 });
 
 
+
+// Route to login a user
+app.get('/logout', async (req, res) => {
+  try {
+      req.session.authenticated=false;
+      res.status(200).send('LogOut successful');
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({ message: 'Error logging in' });
+    }
+});
 
 
 
@@ -239,7 +230,7 @@ app.get('/all-obesity', (req, res) => {
 
 
 // Route to get a single obesity data by ID
-app.get('/single-obesity', authenticateJWT, (req, res) => {
+app.get('/single-obesity', authenticateSession, (req, res) => {
   const id = req.query.id; // Extract the ID from query parameters
 
   ObesityData.findById(id) // Use the extracted ID in the findById method
@@ -272,26 +263,26 @@ app.get('/single-obesity', authenticateJWT, (req, res) => {
 // });
 
 
-app.get('/single-obesity', (req, res) => {
-  const id = req.query.id; // Extract the ID from query parameters
+// app.get('/single-obesity', (req, res) => {
+//   const id = req.query.id; // Extract the ID from query parameters
 
-  ObesityData.findById(id) // Use the extracted ID in the findById method
-    .then((result) => {
-      if (!result) {
-        return res.status(404).send({ message: 'Data not found' });
-      }
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send(err); // Send the error as a response
-    });
-});
+//   ObesityData.findById(id) // Use the extracted ID in the findById method
+//     .then((result) => {
+//       if (!result) {
+//         return res.status(404).send({ message: 'Data not found' });
+//       }
+//       res.send(result);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).send(err); // Send the error as a response
+//     });
+// });
 
 
 
 // Route to update obesity data by ID
-app.put('/update-obesity/:id', authenticateJWT, (req, res) => {
+app.put('/update-obesity/:id', authenticateSession, (req, res) => {
   const id = req.params.id; // Extract the ID from the request parameters
   const updateData = req.body; // Extract the update data from the request body
 
@@ -310,8 +301,7 @@ app.put('/update-obesity/:id', authenticateJWT, (req, res) => {
 });
 
 
-app.get('/filteredSearch', (req, res) => {
-    console.log("filtered Search");
+app.get('/filteredSearchData', authenticateSession, (req, res) => {
     // Retrieve filter parameters from query string
     const { age, gender } = req.query;
 
@@ -341,13 +331,13 @@ app.get('/filteredSearch', (req, res) => {
 //***********  Add Operation
 
 // Route to render login page
-app.get('/add', (req, res) => {
+app.get('/add', authenticateSession, (req, res) => {
   res.render('add');
 });
 
 
 // Register route (POST)
-app.post('/add', async (req, res) => {
+app.post('/add', authenticateSession, async (req, res) => {
   const { Age, 
     Gender, 
     Height, 
@@ -400,153 +390,50 @@ app.post('/add', async (req, res) => {
 
 // ****************** Delete Operations
 
-app.get('/delete/:id', async (req, res) => {
-  const userId = req.params.id;
-
+app.delete('/delete', authenticateSession, async (req, res) => {
+  const obesityId = req.query.id;
+  console.log(obesityId);
   try {
-    const deletedUser = await User.findByIdAndDelete(userId);
-    if (!deletedUser) {
-      return res.status(404).send('User not found!');
+    const deletedData = await ObesityData.findByIdAndDelete(obesityId);
+    if (!deletedData) {
+      return res.status(404).send('Data not found!');
     }
-    res.send('User deleted successfully!'); // Change to a proper response
+    res.send(deletedData); // Change to a proper response
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error deleting user!');
+    res.status(500).send('Error deleting data!');
   }
 });
 
+// Route to update obesity data by ID
+app.put('/update-obesity/:id', authenticateSession, (req, res) => {
+  const id = req.params.id; // Extract the ID from the request parameters
+  const updateData = req.body; // Extract the update data from the request body
 
-// // Route to update obesity data by ID
-// app.put('/update-obesity/:id', (req, res) => {
-//   const id = req.params.id; // Extract the ID from the request parameters
-//   const updateData = req.body; // Extract the update data from the request body
-
-//   // Use findByIdAndUpdate to update the document
-//   ObesityData.findByIdAndUpdate(id, updateData, { new: true })
-//     .then((result) => {
-//       if (!result) {
-//         return res.status(404).send({ message: 'Data not found' });
-//       }
-//       res.send(result);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).send(err); // Send the error as a response
-//     });
-// });
-
-
-// app.put('/update-obesity', (req, res) => {
-//   const id = req.params.id; // Extract the ID from path parameters
-//   const updateData = req.body; // Extract the update data from the request body
-
-//   // Use findByIdAndUpdate to update the document
-//   ObesityData.findByIdAndUpdate(id, updateData, { new: true })
-//     .then((result) => {
-//       if (!result) {
-//         return res.status(404).send({ message: 'Data not found' });
-//       }
-//       res.send(result);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).send(err); // Send the error as a response
-//     });
-// });
-
-// app.get('/about', (req, res) => {
-//   res.render('./pages/about', { title: 'About Sales Blog'});
-// });
-// //redirects
-
-// app.use((req, res)=> {
-//   res.status(404).render('404', { title: 'Page not found' });
-// });
+  // Use findByIdAndUpdate to update the document
+  ObesityData.findByIdAndUpdate(id, updateData, { new: true })
+    .then((result) => {
+      if (!result) {
+        return res.status(404).send({ message: 'Data not found' });
+      }
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send(err); // Send the error as a response
+    });
+});
 
 
-// //Create a route to manage the obesity data entry
-// app.post('/obesity', (req, res) => {
-//   const { age, gender, height, weight, calc, favc, fcvc, ncp, scc, smoke, ch2o, family_history_with_overweight, faf, tue, caec, mtrans, nobeyesdad} = req.body;
-//   const obesity_data = new obesityData({age, gender, height, weight, calc, favc, fcvc, ncp, scc, smoke, ch2o, family_history_with_overweight, faf, tue, caec, mtrans, nobeyesdad});
+// ********************** All data
 
-//   obesity_data.save()
-//         .then((result) => {
-//           res.status(201).json(result);
-//         })
-        
-//         .catch((err) => {
-//           console.error(err);
-//           res.status(500).json({error: 'Error created obesity data'});
-//         });
-
-// });
-
-// //Creating a route to manage the deleting of an entry in obesity data
-// app.delete('/obesity/:id', (req, res) => {
-//   const {id} = req.params;
-
-//   obesityData.findByIdAndDelete(id)
-//         .then(() => {
-//           res.sendStatus(204);
-//         })
-//         .catch((err) => {
-//           console.error(err);
-//           res.status(500).json({error: 'Error deleted obesity data'});
-//         });
-// });
-
-//Listen for the requests
-/* app.listen(3000);
-
-app.get('/', (req, res) => {
-  res.send('<p> home page </p>');
-
-}); */
-
-//npm requirements 'express'
-
-//var express = require('express');
-//var app = express();
-//app.listen(3000, ()=> {
-//  console.log('Server listening on 3000');
-//})
-
-//const mongoose = require('mongoose');
-//mongoose.connect('URL', () => {
-//  console.log('Connected to Mongo DB successfully!!');
-//})
-
-//APIs for a routing mechanism
-//body parser to handle HTTP POST calls
-//const bodyParser = require('body-parser');
-//app.use(bodyParser.urlencoded({extended:true}));
-//app.use(bodyParser.json());
-
-//defining the route with express
-//const router = express.Router();
-//const User = require('model/user.js');
-//const bcrypt = require('bcrypt');
-//router.post('/user', (request, response) => {
- // const user = new User ({
- //   firstname : request.body.firstname,
-  //  lastname: request.body.lastname,
-  //  username: request.body.username,
-//    password: request.body.password,
-//    email: request.body.email
-//  });
-
-//bcrypt.hash(user.password, 10, function(err, hash) {
-//  if(err){
-//    return next(err);
- // }
-
-//user.password = hash;
-//user.save().then(data => {
- // console.log('Successfully created a new user');
-//}).catch(error => {
-
- // })  
-//})
-//}) .module.exports = router
-
+app.get('/allData', authenticateSession, async (req, res) => {
+  try {
+    const obesity = await ObesityData.find();
+    res.render('allData', { obesity }); 
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error fetching data!');
+  }
+});
 
